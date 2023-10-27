77c9410563ce82d9fea11acdfacf38c673b2201d
Gustavo Gil <diemort@gmail.com>
2023-10-27 14:55:56 -0300

    adding bash script for downloading youtube videos for teaching classes with auto convert to mp4 and embedding subs

---
+++ video-down.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# download with pt-BR subs:
+echo "Downloading ${1}"
+yt-dlp --write-sub --write-auto-sub --sub-lang "pt-BR*" ${1}
+filename=$( yt-dlp --write-sub --write-auto-sub --sub-lang "pt-BR*" --get-filename --no-download-archive ${1} )
+# convert preserving quality and subs:
+echo "Converting video to mp4"
+ffmpeg -i "$filename" -crf 17 -c:v libx264 -map 0 -c:a aac -c:s mov_text "$( basename "$filename" .webm )".mp4
+# output
+rm -rf "$filename"
+echo "Download of $( basename "$filename".mp4 ) completed"
+
