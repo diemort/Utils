fd95429d7f6bcce7cab857f2bc902a65bcb31555
Gustavo Gil <diemort@gmail.com>
2023-10-29 22:02:32 -0300

    adding embedded subs, expliciting filename, and correcting line for final message

--- video-dl.sh
+++ video-dl.sh
@@ -2,8 +2,11 @@
 
 # download with pt-BR subs:
 echo "Downloading ${1}"
-yt-dlp --write-sub --write-auto-sub --sub-lang "pt-BR*" ${1}
-filename=$( yt-dlp --write-sub --write-auto-sub --sub-lang "pt-BR*" --get-filename --no-download-archive ${1} )
+yt-dlp --write-sub --write-auto-sub --sub-lang "pt*" --embed-subs ${1}
+
+# extract filename:
+filename=$( yt-dlp --get-filename --no-download-archive ${1} )
+
 # convert preserving quality and subs:
 if [[ $filename != *.mp4 ]]
 then
@@ -11,7 +14,7 @@ then
     ffmpeg -i "$filename" -crf 17 -c:v libx264 -map 0 -c:a aac -c:s mov_text "$( basename "$filename" .webm )".mp4
     # output
     rm -rf "$filename"
-    echo "Download of $( basename "$filename" .webm).mp4 completed"
+    rm -rf $( basename "$filename" .webm ).*.vtt
 fi
-echo "Download of "$filename" completed"
+echo "Download of $( basename "$filename" .webm).mp4 completed"
 
